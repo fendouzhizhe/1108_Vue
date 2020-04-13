144426f8274075ebedca98444a3524710483db58
yanchenxu <1115045919@qq.com>
2020-04-13 13:49:04 +0800

    完成Vue案例

---
+++ my-project/src-自己实现/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    这是组件
+    <Hello/>
+  </div>
+</template>
+<script>
+// 引入Hello组件
+import Hello from './components/Hello'
+export default {
+  name: 'App',
+  components: {
+    Hello
+  }
+}
+</script>
+<style scoped>
+div{
+  width:200px;
+  height:100px;
+  color:red;
+}
+</style>
---
+++ my-project/src-自己实现/components/Hello.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>哈哈哈哈</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Hello'
+}
+</script>
+<style scoped>
+div{
+  width:200px;
+  height:100px;
+  color:green;
+}
+</style>
